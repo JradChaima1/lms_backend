c07e279c2208237947e8ed901b8cd98f7fc8b7e9
JradChaima1 <chaimajrad1888@gmail.com>
2025-11-22 07:38:52 +0000

    add user + course entity and repository

---
+++ src/main/java/com/lms/lms_backend/entity/User.java
@@ -0,0 +1,33 @@
+package com.lms.lmsbackend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private Role role = Role.STUDENT;
+
+    public enum Role {
+        STUDENT,
+        ADMIN
+    }
+}
---
+++ src/main/java/com/lms/lms_backend/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.lms.lmsbackend.repository;
+
+import com.lms.lmsbackend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
